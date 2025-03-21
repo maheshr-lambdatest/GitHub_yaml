337782474a24ba0e526fd96d374ed9a8010a482e
Mahesh <151466093+maheshr-lambdatest@users.noreply.github.com>
2025-03-21 21:51:46 +0530

    Update TestNGTodo1.java

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.By; 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
