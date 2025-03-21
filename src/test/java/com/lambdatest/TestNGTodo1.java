032e615b4b80e90e3f4ff14d090140e2df61f922
Mahesh <151466093+maheshr-lambdatest@users.noreply.github.com>
2025-03-21 22:16:21 +0530

    Update TestNGTodo1.java

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.testng.ITestContext;
+import org.testng.ITestContext; 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
